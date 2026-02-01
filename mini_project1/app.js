dcf4eaa7ec5092ee2f3bbbba8cf4a55f919a67f9
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2026-02-02 02:06:29 +0530

    mini project WIP

--- mini_project1/app.js
+++ mini_project1/app.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-    console.log('Root route accessed');
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/register", async (req, res) => {
+  let { username, name, age, email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) {
+    return res.status(500).send("User already exists");
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      console.log(hash);
+      let user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password: hash,
+      });
+
+      let token = jwt.sign({ email: email, userid: user._id }, "secretkey");
+      res.cookie("token", token);
+      res.send("User registered successfully");
+    });
+  });
 });
 
 app.listen(3000);
-
-
---
+++ mini_project1/readme.md
@@ -0,0 +1,7 @@
+users can write post
+login & register
+logout
+post creation
+post like 
+post delete
+
